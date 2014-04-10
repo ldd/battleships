b2a42ee9c220eb5fc7db468e1815c92c23653e36
Leonardo Florez <leonardoldd@gmail.com>
2014-04-10 00:38:46 -0400

    moving stuff

--- server/namespace.js
+++ server/namespace.js
@@ -4,3 +4,9 @@ gameStream = new Meteor.Stream('game');
 mapStream = new Meteor.Stream('map');
 serverStream = new Meteor.Stream('server');
 gameMessageStream = new Meteor.Stream('gameMessage');
+
+
+gameCollection = new Meteor.Collection('games');
+savedCollection = new Meteor.Collection('savedgames');
+mapCollection = new Meteor.Collection('maps');
+inviteCollection = new Meteor.Collection('invites');
